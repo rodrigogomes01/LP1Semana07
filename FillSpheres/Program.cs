3b25abe86dacc97e542acd4df2e552c353d28fa9
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-26 14:11:39 +0100

    Exercise 1
    Added project FillSpheres

---
+++ FillSpheres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FillSpheres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
