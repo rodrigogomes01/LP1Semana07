f42f15477dba14587680192a85aab65e5afeb06e
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-26 14:36:20 +0100

    Exercise 3
    Created class Color with red, blue, green and alpha

---
+++ FillSpheres/Color.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FillSpheres
+{
+    public class Color
+    {
+        public byte Red;
+        public byte Green;
+        public byte Blue;
+        public byte Alpha;
+
+    }
+
+}
