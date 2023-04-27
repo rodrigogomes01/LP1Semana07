3905d3fbd9adfdffd1553f1083a3ebbeab4d1d16
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-27 23:17:18 +0100

    Exercise 5
    Added new mwthods to class color

--- BetterFillSpheres/Color.cs
+++ BetterFillSpheres/Color.cs
@@ -7,18 +7,13 @@ namespace BetterFillSpheres
 {
     public class Color
     {
-        public byte Red;
-        public byte Green;
-        public byte Blue;
-        public byte Alpha;
-
         // Constructor for each color
         public Color(byte red, byte green, byte blue, byte alpha)
         {
-            Red = red;
-            Green = green;
-            Blue = blue;
-            Alpha = alpha;
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+            this.Alpha = alpha;
         }
 
         // Constructor that accepts each color and puts alpha at 255
@@ -30,51 +25,13 @@ namespace BetterFillSpheres
             Alpha = 255;
         }
 
-        // Getters and setters for each color
-        public byte GetRed()
-        {
-            return Red;
-        }
-
-        public void SetRed(byte red)
-        {
-            Red = red;
-        }
-
-        public byte GetGreen()
-        {
-            return Green;
-        }
-
-        public void SetGreen(byte green)
-        {
-            Green = green;
-        }
-
-        public byte GetBlue()
-        {
-            return Blue;
-        }
-
-        public void SetBlue(byte blue)
-        {
-            Blue = blue;
-        }
-
-        public byte GetAlpha()
-        {
-            return Alpha;
-        }
-
-        public void SetAlpha(byte alpha)
-        {
-            Alpha = alpha;
-        }
+        // Getters and setters
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
 
         // Method GetGrey that returns the average of the RGB values
-        public byte GetGrey()
-        {
-            return (byte)((Red + Green + Blue) / 3);
-        }
+        public byte GetGrey { get => (byte)((Red + Green + Blue) / 3); }
     }
 }
