7ac59df685ca1f03c102a185672ddd994e6b7da6
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-28 14:12:35 +0100

    Exercise 8
    Added MaxScore

--- JustScored/PlayerInfo.cs
+++ JustScored/PlayerInfo.cs
@@ -10,6 +10,21 @@ namespace JustScored
         private float maxScore;
         private int gamesPlayed;
         private int gamesWon;
+
+        public float MaxScore
+        {
+            get { return maxScore; }
+
+            set 
+            { 
+                maxScore = value;
+                // Set max score if it is higher than the current max score
+                if (value > maxScore)
+                {
+                    maxScore = value;
+                }
+            }
+        }
     }
 }
 
