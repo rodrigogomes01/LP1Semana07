c3877c6b952e527f5e5287014f7a28d73b569406
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-04-28 14:10:18 +0100

    Exercise 8
    Added project JustScored and created class PlayerInfo

---
+++ JustScored/PlayerInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustScored
+{
+    public class PlayerInfo
+    {
+        private float maxScore;
+        private int gamesPlayed;
+        private int gamesWon;
+    }
+}
+
+
+
+
---
+++ JustScored/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace JustScored
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            PlayerInfo[] playerInfos = {
+                new PlayerInfo("Ana"),
+                new PlayerInfo("Zé"),
+                new PlayerInfo("Doesn't play")
+            };
+
+            playerInfos[0].AddGame(false);
+            playerInfos[0].AddGame(true);
+            playerInfos[0].AddGame(true);
+            playerInfos[0].AddGame(false);
+            playerInfos[0].AddGame(false);
+            playerInfos[0].AddGame(true);
+            playerInfos[0].MaxScore = 123;
+            playerInfos[0].MaxScore = 40;
+
+            playerInfos[1].AddGame(true);
+            playerInfos[1].AddGame(true);
+            playerInfos[1].MaxScore = 12;
+            playerInfos[1].MaxScore = 67;
+            playerInfos[1].MaxScore = 91;
+            playerInfos[1].MaxScore = 32;
+
+            foreach (PlayerInfo playerInfo in playerInfos)
+            {
+                Console.WriteLine($" Player name : {playerInfo.Name}");
+                Console.WriteLine($"    Win rate : {playerInfo.RateOfSuccess}");
+                Console.WriteLine($"  High score : {playerInfo.MaxScore}");
+                Console.WriteLine("--------------------------------");
+            }
+
+            // Output deve ser:
+            //
+            //  Player name : Ana
+            //     Win rate : 0.5
+            //   High score : 123
+            // --------------------------------
+            //  Player name : Zé
+            //     Win rate : 1
+            //   High score : 91
+            // --------------------------------
+            //  Player name : Doesn't play
+            //     Win rate : 0
+            //   High score : 0
+            // --------------------------------
+
+        }
+    }
+}
